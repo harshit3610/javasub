c616af70fa305c0636268291f03a3351678853ab
harshit3610 <harshitshukla36@gmail.com>
2020-07-08 17:21:48 +0530

    MY BOY STATEMENT

--- hello.java
+++ hello.java
@@ -13,5 +13,6 @@ class hello{
     public static void main(String[] args) {
         System.out.println("Hello World!");   
         System.out.println(Operations.factorial(5)); 
+        System.out.println("THIS IS A DEMO MY BOY!");
     }
 }
